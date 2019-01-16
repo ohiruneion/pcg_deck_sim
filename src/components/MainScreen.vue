19c74fd294b79e13fe9a219e810e7d546d688862
ohiruneion <ohiruneion@gmail.com>
2019-01-16 14:10:20 +0900

    b

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -10,6 +10,15 @@
           v-model.number="card.quantity"
           min="0"
           max="4">
+        <input type="radio"  value="pokemon" v-model="card.type">ポケモン
+        <input type="radio"  value="goods" v-model="card.type">グッズ
+        <input type="radio"  value="support" v-model="card.type">サポート
+        <input type="radio" value="stadium" v-model="card.type">スタジアム
+        <input type="radio" value="energy" v-model="card.type">エネルギー
+        <button
+          @click="deleteCard(index)">
+          Delete
+        </button>
       </div>
     </template>
     <button
@@ -17,7 +26,10 @@
       Add
     </button>
     <div>
-      {{'total:' + totalQuantity}}
+      {{'合計：' + totalQuantity + '枚'}}
+    </div>
+    <div>
+      {{'ポケモン：' + totalPokemon + '枚'}}
     </div>
   </div>
 </template>
@@ -44,6 +56,27 @@ export default {
       })
 
       return sum
+    },
+    totalPokemon () {
+      var sum = 0
+
+      this.deck.forEach(function (elm) {
+        sum += elm.quantity
+      })
+
+      return sum
+    },
+    totalGoods () {
+
+    },
+    totalSupport () {
+
+    },
+    totalStadium () {
+
+    },
+    totalEnergy () {
+
     }
   },
   methods: {
@@ -52,6 +85,9 @@ export default {
         name: '',
         quantity: 0
       })
+    },
+    deleteCard (index) {
+      this.deck.splice(index, 1)
     }
   }
 }
