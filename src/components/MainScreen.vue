df6e8f4c335e3bf900fd5331182ddf66b345fcd6
ohiruneion <ohiruneion@gmail.com>
2019-01-16 12:35:49 +0900

    a

---
+++ src/components/MainScreen.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <template v-for="(card, index) in deck">
+      <div :key="index">
+        <input
+          type="text"
+          v-model="card.name">
+        <input
+          type="number"
+          v-model.number="card.quantity"
+          min="0"
+          max="4">
+      </div>
+    </template>
+    <button
+      @click="addCard()">
+      Add
+    </button>
+    <div>
+      {{'total:' + totalQuantity}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainScreen',
+  data () {
+    return {
+      deck: [
+        {
+          name: '',
+          quantity: 0
+        }
+      ]
+    }
+  },
+  computed: {
+    totalQuantity () {
+      var sum = 0
+
+      this.deck.forEach(function (elm) {
+        sum += elm.quantity
+      })
+
+      return sum
+    }
+  },
+  methods: {
+    addCard () {
+      this.deck.push({
+        name: '',
+        quantity: 0
+      })
+    }
+  }
+}
+</script>
+
+<style scoped></style>
